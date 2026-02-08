f19652af5c57f65249d1920096a7761c03a19e07
Sorin <sorin.cirneala@gmail.com>
2026-02-08 22:14:40 +0200

    add twitch basic test

---
+++ test_twitch_streamer.py
@@ -0,0 +1,33 @@
+import pytest
+from playwright.sync_api import Page
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args, playwright):
+    return {
+        **browser_context_args,
+        **playwright.devices["Galaxy S24"],
+    }
+
+
+def test_twitch_streamer(page: Page):
+    viewport_size = page.viewport_size
+    assert viewport_size["width"] == 360  # S24 screen width
+
+    page.goto("https://www.twitch.tv")
+
+    page.get_by_role("button", name="Keep using web").click()
+    page.get_by_role("button", name="Accept").click()
+
+    page.get_by_role("link", name="Browse").click()
+    page.get_by_role("searchbox", name="Search").click()
+    page.get_by_role("searchbox", name="Search").fill("starcraft ii")
+    page.get_by_role("searchbox", name="Search").press("Enter")
+    page.screenshot(path="1_before_scroll.png")
+    page.mouse.wheel(0, 200)
+    page.screenshot(path="2_after_scroll.png")
+    live_streamers = page.get_by_role("button", name="Live").all()
+    print(live_streamers)
+
+    live_streamers[0].click()
+    page.screenshot(path="3_streamer_page.png")
