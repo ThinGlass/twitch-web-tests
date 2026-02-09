7e0ddab058fd0e71ad1167a08351375f34fa7d44
Sorin <sorin.cirneala@gmail.com>
2026-02-09 20:22:56 +0200

    update testfile

--- test_twitch_streamer.py
+++ test_twitch_streamer.py
@@ -1,15 +1,9 @@
 import pytest
+
 from playwright.sync_api import Page
 
 
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args, playwright):
-    return {
-        **browser_context_args,
-        **playwright.devices["Galaxy S24"],
-    }
-
-
+@pytest.mark.smoke
 def test_twitch_streamer(page: Page):
     viewport_size = page.viewport_size
     assert viewport_size["width"] == 360  # S24 screen width
