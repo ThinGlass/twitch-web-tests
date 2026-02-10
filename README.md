0bc0caa38addf51049909b2d29ed514083cb7fa3
Sorin <sorin.cirneala@gmail.com>
2026-02-10 19:17:43 +0200

    add allure reporting, move all images to separate folder

--- README.md
+++ README.md
@@ -4,9 +4,13 @@
 
 1. Create a virtual environment
 2. Install dependencies from requirements.txt
-3. Run the test in command line:
+3. Install the playwright browsers:
 
-   `pytest --headed --slowmo 2000`
+   `playwright install`
+
+4. Run the test in command line:
+
+   `pytest --headed --alluredir=allure-report`
 
 ## Features
 
@@ -15,8 +19,12 @@
   - Locators are stored in the class, but they can be moved to a separate file when more tests and pages are added
 - Test preconditions should be implemented as fixtures, to allow re-using them when needed
 - Logging enabled - both for CLI (debugging) and for captured logs (pipelines)
-- Reporting not enabled here, but can be implemented as HTML report or Allure. See the API framework for basic HTML.
+- Report is generated with Allure
 
 ## Test execution GIF
 
-![test execution GIF](test_execution.gif)
+![test execution GIF](images/test_execution.gif)
+
+## Allure report screenshot
+
+![Allure report](images/allure_report.png)
--- tests/test_twitch_streamer.py
+++ tests/test_twitch_streamer.py
@@ -1,26 +1,41 @@
 import pytest
+import allure
 
 from playwright.sync_api import Page
 from pages.twitch_home import TwitchHomePage
 from pages.categories_page import CategoriesPage
 
 
-@pytest.mark.debug
+@pytest.mark.smoke
 def test_twitch_streamer(page: Page):
-    viewport_size = page.viewport_size
-    assert viewport_size["width"] == 360  # S24 screen width
+    with allure.step("Check the viewport size"):
+        viewport_size = page.viewport_size
+        assert viewport_size["width"] == 360  # S24 screen width
 
     # home page interactions
-    home_page = TwitchHomePage(page)
-    home_page.navigate()
-    home_page.accept_keep_using_web()
-    home_page.accept_cookies()
-    home_page.click_browse()
+    with allure.step("Open Twitch page"):
+        home_page = TwitchHomePage(page)
+        home_page.navigate()
+        assert home_page.page.title() == "Twitch"
 
-    # categories page interactions
-    categories_page = CategoriesPage(page)
-    categories_page.search_for_game("starcraft ii")
-    categories_page.page.mouse.wheel(0, 200)
-    streamers = categories_page.get_all_streamers()
-    streamers[0].click()
-    categories_page.take_screenshot()
+    with allure.step("Stay in browser and accept cookies"):
+        home_page.accept_keep_using_web()
+        home_page.accept_cookies()
+
+    with allure.step("Open the Categories page"):
+        home_page.click_browse()
+        categories_page = CategoriesPage(page)
+
+    with allure.step("Search for Starcrat II"):
+        categories_page.search_for_game("starcraft ii")
+
+    with allure.step("Scroll down"):
+        categories_page.page.mouse.wheel(0, 200)
+
+    with allure.step("Click on the first streamer"):
+        streamers = categories_page.get_all_streamers()
+        assert len(streamers) > 0
+        streamers[0].click()
+
+    with allure.step("Take screenshot"):
+        categories_page.take_screenshot()
